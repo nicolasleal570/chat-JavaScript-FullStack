3c637deb5e2a0ac6f4eab4fd8afdc1afc2517238
nicolasleal570 <lealnicolas96@gmail.com>
2018-05-31 21:51:46 -0400

    Agregando las funciones para tener usuarios (sin DB)

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,12 +5,39 @@ $(document).ready(function(){
 	*/
 	const socket = io();
 
-	/* Obteniendo los elementos del DOM de la interfaz */
+	/* Obteniendo los elementos del DOM de la interfaz de chat */
 	const messageForm = $("#message-form");
 	const messageBox = $("#message");
 	const chat = $("#chat");
 
+	/* Obtiendo los elementos del DOM para iniciar sesion */
+	const nickForm = $("#nickForm");
+	const nickName = $("#nickName");
+	const nickError = $("#nickError");
+
+	/* Obteniendo los elementos del DOM para imprimir la lista de usuarios conectados al chat */
+	const users = $("#usernames");
+
 	/* ----- */
+
+	nickForm.submit(e => {
+		e.preventDefault();
+
+		/* Enviando los datos del usuario al servidor */
+		socket.emit('new user', nickName.val(), function (data) {
+			if (data) {
+				$("#nickWrap").hide();
+				$('#contentWrap').show();
+			}else{
+				nickError.html(`
+					<div class="alert alert-danger mt-2">
+						That username already exits.
+					</div>
+				`);
+			}
+			nickName.val('');
+		});
+	});
 
 	/* Eventos */
 	messageForm.submit(function (e) {
@@ -22,8 +49,18 @@ $(document).ready(function(){
 	});
 
 	/* Escuchando los eventos del servidor */
-	socket.on('new message', function (data) {		
-		chat.append(data) + '<br/>';
+	socket.on('new message', function (data) {	
+		chat.append('<b>' + data.nick + ': </b>' + data.msg + '<br/>')
+	});
+
+	socket.on('usernames', data => {
+		let html = '';
+
+		for (let i = 0; i < data.length; i++) {
+			html += `<p><i class="fas fa-user"></i> ${data[i]} </p>`
+			
+		}
+		users.html(html);
 	});
 
 
--- src/sockets.js
+++ src/sockets.js
@@ -4,15 +4,51 @@
  * 
  */
 module.exports = function (io) {
+
+	/* Usuarios Conectados */
+	let nickNames = [];
+
 	io.on('connection', socket => {
 		console.log("Nuevo usuario conectado");
 
+		socket.on('new user', (data, callb) => {
+			if (nickNames.indexOf(data) != -1) {
+				/* El usuario ya existe */
+				callb(false);
+			}else{
+				callb(true);
+				/* Guardando el nickname en la conexion de socket */
+				socket.nickname = data;
+				nickNames.push(socket.nickname);
+
+				io.sockets.emit('usernames', nickNames);
+			}
+		});
+
 		/* Escuchando el evento de la conexion de socket para enviar los mensajes del cliente */
 		socket.on('send message', function (data) {
+			io.sockets.emit('new message', {
+				msg: data,
+				nick: socket.nickname
+			});
 
 			/* Enviando el mensaje a todos los usuarios conectados */
-			io.sockets.emit('new message', data);
+			updateNicknames();
 		});
+
+		/* Escuchando cuando se desconecta un usuario */
+		socket.on('disconnect', data => {
+			if (!socket.nickname) return;
+
+			/* Borrando a usuario del arreglo */
+			nickNames.splice(nickNames.indexOf(socket.nickname), 1);
+			updateNicknames();
+		});
+
+		/* FUncion para actualizar nombre de usuario */
+		function updateNicknames() {
+			io.sockets.emit('usernames', nickNames);
+		}
 
 	});
 }
